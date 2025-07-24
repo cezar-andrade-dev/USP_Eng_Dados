56c6bbc926c08a9e6181e097375da374d1dddeb3
cezar-andrade-dev <cezar.andrade.dev@gmail.com>
2025-07-23 23:12:42 -0300

    Inserção de novos arquivos sql

---
+++ aula2.sql
@@ -0,0 +1,14 @@
+create database livraria;
+use livraria;
+
+create table autores(
+	id  int primary key auto_increment,
+    nome varchar(200)
+    );
+
+create	table generos(
+	id int primary key auto_increment,
+    nome varchar(200)
+);
+
+
